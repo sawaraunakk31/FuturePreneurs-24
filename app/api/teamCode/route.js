74a4b490889fa342f68158ae3246b91cd4019c19
ThisIsIAmAryan <aryanshar17@gmail.com>
2024-09-10 23:22:39 +0530

    TeamCode Generation API

--- app/api/teamCode/route.js
+++ app/api/teamCode/route.js
@@ -2,7 +2,7 @@
 
 export async function GET(request) {
     // Generate an 8-digit alphanumeric code
-    const teamCode = Math.random().toString(36).substring(2, 10).toUpperCase(); // Generates a random 8-character code
+    const teamCode = Math.random().toString(36).substring(2, 10); // Generates a random 8-character code
     
     return new Response(JSON.stringify({ code: teamCode }), {
         status: 200,
