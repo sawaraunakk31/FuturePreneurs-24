ea7407a401bf26b1b29d3dca4a39844d9d454df5
Sujal-tyagi17 <tyagisujal007@gmail.com>
2024-09-15 01:55:13 +0530

    "Updated package-lock.json and package.json with changes to dependencies and versions"

--- app/api/teamCode/route.js
+++ app/api/teamCode/route.js
@@ -1,8 +1,6 @@
-// app/api/teamCode/route.js
 
 export async function GET(request) {
-    // Generate an 8-digit alphanumeric code
-    const teamCode = Math.random().toString(36).substring(2, 10); // Generates a random 8-character code
+    const teamCode = Math.random().toString(36).substring(2, 10); 
     
     return new Response(JSON.stringify({ code: teamCode }), {
         status: 200,
