0b5871dac302a28fff38d0e6acfe4b25395dbc88
KAVYAJAIN410 <kavyajain@KAVYAs-MacBook-Air.local>
2024-09-14 20:13:27 +0530

    user details

--- app/page.js
+++ app/page.js
@@ -1,21 +1,16 @@
 "use client";
 import Navbar from '../components/Navbar';
-import React, { useState } from 'react';
+import React from 'react';
 import Link from "next/link";
-import Image from 'next/image';
-import UserDetail from './userDetails/user.js'
-export default function page() {
+import UserDetail from './userDetails/user.js'; // Ensure this path is correct
+
+export default function Page() {
   return (
     <main>
       <Navbar />
       <div>Futurepreneurs 10.0</div>
       <Link href={'/userDetails'}>Sign In</Link>
-      <UserDetail></UserDetail>
+      <UserDetail />
     </main>
   );
 }
-
-
-
-
-
