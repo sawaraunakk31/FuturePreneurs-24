5a5b2e05477436a05902869043d7389284f126a4
Sujal-tyagi17 <tyagisujal007@gmail.com>
2024-09-21 13:39:47 +0530

    teamDetailsupdate

--- app/teamDetails/page.js
+++ app/teamDetails/page.js
@@ -85,9 +85,9 @@ const TeamDetails = () => {
     };
 
   return (
-    <div className="h-[100vh] flex items-center justify-center text-white font-sans bg-[url('../assests/assests/background_image.jpg')] bg-cover bg-center">
+    <div className="h-[100vh] w-full flex flex-col items-center justify-center text-white font-sans bg-white bg-cover bg-center">
       {loading && <LoadingScreen />}
-      <div className="w-[90%] max-w-[20rem] md:w-[95%] md:max-w-[28rem] lg:max-w-[32rem] p-[1.5rem] md:p-[2rem] bg-gray-800 rounded-lg shadow-lg flex flex-col justify-between md:h-[32rem] lg:h-[26rem]">
+      <div className="w-[90%] max-w-[20rem] md:w-[95%] md:max-w-[32rem] lg:max-w-[32rem] p-[1.5rem] md:p-[2rem] bg-gray-800 rounded-lg shadow-lg flex flex-col justify-center md:h-[20rem] lg:h-[18rem] sm:max-w-[20rem] items-centre">
         <div className="text-center mb-[1.5rem]">
           <h1 className="text-[1.75rem] md:text-[2.5rem] lg:text-[3rem] font-bold pb-[0.5rem]">
             {teamName}
@@ -95,12 +95,12 @@ const TeamDetails = () => {
         </div>
 
         <div className="flex items-center justify-start md:justify-between mb-[1.25rem] whitespace-nowrap">
-          <span className="text-[1.125rem] md:text-[1.25rem] ml-[1rem]">
+          <span className="text-[1.5rem] sm:text-[1.8rem] md:text-[2rem] ml-[1rem] sm:ml-[1rem] sm:mr-[1rem]">
             {teamCode}
           </span>
           <button
-            className="text-[0.875rem] md:text-[1.125rem] text-gray-400 hover:text-white flex items-center ml-auto md:ml-0 mr-[1rem]"
-            onClick={() => handleCopy(teamCode)}
+            className="text-[1rem] sm:text-[1.8rem] md:text-[2rem] text-gray-400 hover:text-white flex items-center ml-auto md:ml-0 mr-[0.5rem]"
+            onClick={() => handleCopy(teamCode)} 
           >
             <FontAwesomeIcon className="mr-[0.5rem]" icon={faCopy} />
             Click here to copy
@@ -108,7 +108,7 @@ const TeamDetails = () => {
         </div>
 
         <div className="text-center font-bold">
-          <h2 className="text-[1.5rem] md:text-[2rem] lg:text-[2rem] pb-[0.75rem]">
+          <h2 className="text-[1.5rem] sm:text-[1.25rem] md:text-[2rem] lg:text-[2rem] pb-[0.75rem] whitespace-nowrap">
             Share with Members!
           </h2>
         </div>
