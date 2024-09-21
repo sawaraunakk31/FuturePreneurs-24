73716b5c69877a5e20e4e38fa98661f1854ca44c
KV225511 <krissh.verma2023@vitstudent.ac.in>
2024-09-22 01:09:54 +0530

    changes to faq

--- app/page.js
+++ app/page.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import HeroSection from '@/components/landingPage/heroSection';
 import Animation from '@/components/landingPage/animation';
 import Timeline from '@/components/landingPage/timeline';
+import Faqpage from '@/components/FAQcomp/faqpage';
 
 export default function page() {
   return (
     <main className='min-h-screen'>
     <Animation/>
-
     <Timeline />
+    <Faqpage/>
     </main>
   )
 }
