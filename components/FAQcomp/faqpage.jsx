aa5f7ad6e795a6c2405a94c95ed52037ce8f466e
KV225511 <krissh.verma2023@vitstudent.ac.in>
2024-09-24 02:40:07 +0530

    some more changes(1)

--- components/FAQcomp/faqpage.jsx
+++ components/FAQcomp/faqpage.jsx
@@ -1,7 +1,6 @@
 
 import FaqHeader from "./FaqHeader";
 import Footer from "@/components/footer";
-import faq from '@/public/assests/assests/FAQs_background.png'
 import FaqMainContent from "@/components/FAQcomp/FaqMainContent";
 import Footersm from "../footersm";
 
@@ -9,14 +8,10 @@ export default function Faqpage(){
     return(
         <>
         <main className="overflow-auto ">
-            
         <FaqHeader/>
-         <div className="bg-[url('../../public/assests/assests/FAQs_background.png')">
-            {/* <img src={faq} className="-z-10" /> */}
-        <FaqMainContent/>
-         </div>   
-        <Footer className="hidden md:flex"/>
-        <Footersm className="flex"/>
+        <FaqMainContent/>  
+        <div className="hidden md:block"><Footer/></div>
+        <div className="block md:hidden"><Footersm/></div>
         </main>
         </>
     )
