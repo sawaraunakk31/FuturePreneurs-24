88b92be5c7b924716b8b236d8aa12b08a9889892
KV225511 <krissh.verma2023@vitstudent.ac.in>
2024-09-24 23:34:53 +0530

    changes done to faq(1)

--- components/footersm.jsx
+++ components/footersm.jsx
@@ -21,7 +21,7 @@ const Footersm=()=>{
               </h1>
             </div>
             <div className="flex flex-col gap-3">
-                <div className='flex  items-center justify-start text-[#FFFFFF] gap-2'>
+                <div className='flex  items-center justify-start text-[#FFFFFF] gap-2 pb-2px'>
                     <p className='block sm:hidden'><PhoneIconsm  /></p>
                     <p className='hidden sm:block' ><PhoneIcon /></p>
                   <a href="tel:+91 87777 93331" className="text-white text-[3vw]">+91 87777 93331</a>
@@ -33,7 +33,7 @@ const Footersm=()=>{
                 </div>
           </div>
         </div>
-          <div className="flex justify-around items-center flex-1">
+          <div className="flex justify-around items-center flex-1 p-5">
             <div className='flex flex-col gap-[2vh]'>
                 <div>
                 <a href="https://www.instagram.com/ecell_vit/" className="p-1   flex items-center" style={{Image:'url("@/assests/assests/socialmediaborder.png")'}} target="_blank" rel="noopener noreferrer">
@@ -57,7 +57,7 @@ const Footersm=()=>{
                 </div>
             </div>
             <div>
-                <p className='text-sm justify-center items-center text-[rgba(255,255,255,0.61)] flex w-[70vw] text-justify pt-10'>
+                <p className='text-sm justify-center items-center text-[rgba(255,255,255,0.61)] flex w-[70vw] text-justify '>
                 The Entrepreneurship Cell, VIT Vellore, is a student-led organization established to promote the spirit of entrepreneurship among young innovators. E-Cell aims to provide students with a platform along with the necessary tools to convert their ideas into successful business ventures. The club’s mission is to create an engaging entrepreneurial culture within the institution while establishing a network of various investors, mentors, and evaluators to enhance the startup ecosystem for the future. 
                 </p>
             </div>
