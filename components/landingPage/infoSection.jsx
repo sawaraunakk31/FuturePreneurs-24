330f7d2139ef40d1a59826c59ca1e7eb763e1e30
puniti-web <punitijodhwani29@gmail.com>
2024-09-25 00:52:48 +0530

    info section

--- components/landingPage/infoSection.jsx
+++ components/landingPage/infoSection.jsx
@@ -12,13 +12,19 @@ export default function InfoSection() {
         <h1 className="text-3xl md:text-7xl font-bold mb-6 text-black">Business Simulation Game</h1>
       </div>
       
-      <div className="flex flex-col md:flex-row items-center justify-center max-w-6xl mx-auto flex-grow">
+      <div className="flex flex-col md:flex-row items-center justify-center max-w-8xl mx-auto flex-grow px-4 md:px-8">
+
         <div className="w-full md:w-1/2 mb-8 md:mb-0 flex justify-center">
-          <Image src={logoFP} alt="FuturePreneurs Logo" width={550} height={575} className="max-w-full h-auto" />
+          <Image src={logoFP} alt="FuturePreneurs Logo" width={550} height={570}  className="max-w-full h-auto" />
         </div>
         <div className="w-full md:w-1/2 text-center md:text-left md:pl-8">
           <p className="text-lg md:text-2xl mb-6 text-gray-700 pb-7 md:pb-0 text-justify">
-          Propelled by the passion and hard work of E-Cell, VIT, we are elated to bring you the 10th edition of Futurepreneurs. The flagship event of E-Cell, VIT, reflecting its dedication to promoting innovation and entrepreneurship.This event is designed to offer to all contestants, a hands-on experience with business simulations. Participants will be presented with challenging scenarios and situations that mirror real-life dilemmas, formulated to test their management and decision-making abilities. Every contestant is bound to leave the event better equipped to tackle real-world problems and make informed business decisions in the future. Futurepreneurs 10.0 promises to be an engaging and enriching experience for all who are a part of it. Futurepreneurs 10.0 is set to deliver a captivating and rewarding experience for everyone involved.
+          Propelled by the passion and hard work of E-Cell, VIT, we are elated to bring you the 10<sup>th</sup> edition of Futurepreneurs, the flagship event of E-Cell, VIT, which reflects its dedication to promoting innovation and entrepreneurship. This event is designed to offer to all contestants a hands-on experience with business simulations. Participants will face challenging scenarios designed to test their management and decision-making abilities.
+
+          </p>
+          <p className="text-lg md:text-2xl mb-6 text-gray-700 pb-7 md:pb-0 text-justify">
+          Every contestant is bound to leave the event better equipped to tackle real-world problems and make informed business decisions in the future. Futurepreneurs 10.0 promises to be an engaging, enriching, captivating, and rewarding experience for all those involved.
+
           </p>
         </div>
       </div>
