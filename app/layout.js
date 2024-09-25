abb3e34c82654c3b666f4f284be736d20798e2d0
kv8gh <vyaskaran1409@gmail.com>
2024-09-25 19:04:55 +0530

    speed testing

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import { AuthProvider } from "@/libs/Provider";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -31,6 +32,7 @@ export default function RootLayout({ children }) {
       <AuthProvider>
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
           {children}
+          <SpeedInsights/>
         </body>
       </AuthProvider>
     </html>
