72e512f5350fa41ae5ef07028573cb815b1e9f82
kv8gh <vyaskaran1409@gmail.com>
2024-10-02 14:07:47 +0530

    register's closed

--- components/registerButton.jsx
+++ components/registerButton.jsx
@@ -65,11 +65,12 @@ const registerButton = () => {
         <button
           className="bg-gradient-to-r from-purple-500 to-blue-500 text-white font-semibold py-3 px-10 rounded-lg shadow-lg hover:shadow-xl transition duration-300 hover:text-black"
           onClick={() => {
-            setLoading(true);
-            signIn("google");
+            router.push('/');
+            // setLoading(true);
+            // signIn("google");
           }}
         >
-          {loading ? <LoadingIcons.Oval /> : "Register!"}
+          {loading ? <LoadingIcons.Oval /> : "Register's Closed!"}
         </button>
       ) : (
         <button
